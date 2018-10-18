557955b4b8dc78d6af3e0130fe3693d48cbdd1e3
Sylvia Li <neerGGreen@live.com>
2018-10-18 17:47:37 +0800

    折腾一天字体

--- scripts/add-filters.js
+++ scripts/add-filters.js
@@ -4,7 +4,8 @@
 // env.addFilter('shorten', function(str, count) {
 //     return str.slice(0, count || 5);
 // });
-hexo.extend.filter.register('shorten', function(args){
-    var id = args[0];
-    return str.slice(0, count || 5);
-});
+// hexo.extend.filter.register('shorten', function(args){
+//     var id = args[0];
+//     return str.slice(0, count || 5);
+// });
+console.log(111);
